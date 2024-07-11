3950cb08823dc562e806237ff8e2281a7df308d2
khanhnx2 <nguyenkhanhvietnam@gmail.com>
2024-07-12 02:11:33 +0700

    cm

--- s1_connectgit.sh
+++ s1_connectgit.sh
@@ -4,8 +4,8 @@ ssh-keygen -t rsa -b 4096 -C "quayvaquyt@gmail.com"
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_rsa.pub
 cp ~/.ssh/id_rsa.pub /usr/www/tialia/staticfiles/git.txt
-cp nginx /etc/nginx/sites-available/tialia
-sudo ln -s /etc/nginx/sites-available/tialia /etc/nginx/sites-enabled/
+cp nginx /etc/nginx/sites-available/tialia.service
+sudo ln -s /etc/nginx/sites-available/tialia.service /etc/nginx/sites-enabled/
 sudo nginx -t
 sudo systemctl restart nginx
 echo "Echo check :8000/staticfiles/git.txt"
