fc400d2a53fbf34b90c3855b4fda7ec0684a3fe5
khanhnx2 <nguyenkhanhvietnam@gmail.com>
2024-07-12 01:20:52 +0700

    cm
    cm

--- s2_connectgit.sh
+++ s2_connectgit.sh
@@ -4,7 +4,3 @@ ssh-keygen -t rsa -b 4096 -C "quayvaquyt@gmail.com"
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_rsa.pub
 cp ~/.ssh/id_rsa.pub /usr/www/tialia/id_rsa.pub
-git remote add origin https://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@github.com/quayvaquyt/tialia.git
-git add id_rsa.pub
-git commit -m "id_rsa"
-git push -u origin master
