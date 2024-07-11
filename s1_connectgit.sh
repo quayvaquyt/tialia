60207e4185087298d794b94d0ba974e7756d4a17
khanhnx2 <nguyenkhanhvietnam@gmail.com>
2024-07-12 02:00:36 +0700

    cm

--- s1_connectgit.sh
+++ s1_connectgit.sh
@@ -3,10 +3,10 @@ git config --global user.email "quayvaquyt@gmail.com"
 ssh-keygen -t rsa -b 4096 -C "quayvaquyt@gmail.com"
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_rsa.pub
-cp ~/.ssh/id_rsa.pub /usr/www/tialia/static/git.txt
+cp ~/.ssh/id_rsa.pub /usr/www/tialia/staticfiles/git.txt
 cp nginx /etc/nginx/sites-available/tialia
 sudo ln -s /etc/nginx/sites-available/tialia /etc/nginx/sites-enabled/
 sudo nginx -t
 sudo systemctl restart nginx
-echo "Echo check :8000/static/git.txt
+echo "Echo check :8000/staticfiles/git.txt
 python3 manage.py runserver
