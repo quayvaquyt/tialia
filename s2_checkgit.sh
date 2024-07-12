098f940e1cbd2d17492603775df277b028994f29
quayvaquyt <quayvaquyt@gmail.com>
2024-07-12 11:41:01 +0700

    Update s2_checkgit.sh

--- s2_checkgit.sh
+++ s2_checkgit.sh
@@ -1,1 +1,9 @@
 ssh -T git@github.com
+read -p "Remove tialia config? y|n: " $REMOVE
+if [ "$REMOVE" == "y" ]; then
+    rm -f /etc/nginx/sites-available/tialia.service
+    rm -f /etc/nginx/sites-enabled/tialia.service
+    sudo nginx -t
+    sudo systemctl restart nginx
+    echo "Removed tialia.service"
+fi
