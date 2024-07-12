03acc8172bfbc6c886ea370dde5ed0afc3f9ec02
quayvaquyt <quayvaquyt@gmail.com>
2024-07-12 08:56:54 +0700

    cm

--- s3_pullproject.sh
+++ s3_pullproject.sh
@@ -1,1 +1,5 @@
+read -p "Git Acc: " GIT_ACC
 read -p "Project Folder Name: " PROJECT_FOLDER_NAME
+cd /usr/www
+REPOSITORY = "https://github.com/$GIT_ACC/$PROJECT_FOLDER_NAME.git"
+git clone "$REPOSITORY"
