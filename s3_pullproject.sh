54f7e305d04386657d744686e6d1f3176387a099
quayvaquyt <quayvaquyt@gmail.com>
2024-07-12 09:28:09 +0700

    cm

--- s3_pullproject.sh
+++ s3_pullproject.sh
@@ -4,5 +4,5 @@ read -p "Domain: " DOMAIN
 cd /usr/www/
 git clone https://github.com/$GIT_ACC/$PROJECT_FOLDER_NAME.git
 cd /etc/nginx/sites-available/
-cp /usr/www/$PROJECT_FOLDER_NAME/setting/sites_available $DOMAIN
+cp /usr/www/$PROJECT_FOLDER_NAME/setting/$DOMAIN $DOMAIN
 sudo ln -s /etc/nginx/sites-available/$DOMAIN /etc/nginx/sites-enabled/
