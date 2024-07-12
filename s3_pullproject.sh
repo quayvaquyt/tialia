809a64989f0dc3d8686acf37a9d9b56b883a74e4
quayvaquyt <quayvaquyt@gmail.com>
2024-07-12 09:39:29 +0700

    cm

--- s3_pullproject.sh
+++ s3_pullproject.sh
@@ -1,8 +1,28 @@
 read -p "Git Acc: " GIT_ACC
 read -p "Project Folder Name: " PROJECT_FOLDER_NAME
 read -p "Domain: " DOMAIN
+read -p "Local Port: " LOCAL_PORT
+read -p "Inter Port: " INTER_PORT
 cd /usr/www/
 git clone https://github.com/$GIT_ACC/$PROJECT_FOLDER_NAME.git
 cd /etc/nginx/sites-available/
-cp /usr/www/$PROJECT_FOLDER_NAME/setting/$DOMAIN $DOMAIN
+cat <<EOL > $DOMAIN
+server {
+    listen $INTER_PORT;
+    server_name $DOMAIN;
+
+    location / {
+        proxy_pass http://127.0.0.1:$LOCAL_PORT;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+    }
+
+    location /static/ {
+        alias /usr/www/$PROJECT_FOLDER_NAME/static/;
+    }
+}
+EOL
+echo  "Created NGINX Config for $DOMAIN"
 sudo ln -s /etc/nginx/sites-available/$DOMAIN /etc/nginx/sites-enabled/
